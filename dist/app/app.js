fc91d7a0aec53f7e4ee5d8cc2ebbfdad164e36f1
Shovon96 <fakhruddinshovon@gmail.com>
2025-06-02 01:18:43 +0600

    get all todos and post new todo

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,8 +1,29 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 const express = require('express');
 const app = express();
+// Json File Path
+const dataFilePath = path_1.default.join(__dirname, '../../db/todoData.json');
+// Middleware
+app.use(express.json());
+// Main Route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Learnig Express Js');
+});
+// Get All Todos Data
+app.get('/all-todos', (req, res) => {
+    const data = fs_1.default.readFileSync(dataFilePath, { encoding: 'utf-8' });
+    res.json(data);
+});
+// Post Or Create New Todos
+app.post('/create-todo', (req, res) => {
+    const { title, body } = req.body;
+    console.log(title, body);
+    res.send("todo created");
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,31 @@
 import { Application, Request, Response } from "express"
+import fs from 'fs'
+import path from "path"
 const express = require('express')
-const app : Application = express()
+const app: Application = express()
 
-app.get('/', (req : Request, res : Response) => {
-  res.send('Hello World!')
+// Json File Path
+const dataFilePath = path.join(__dirname, '../../db/todoData.json');
+
+// Middleware
+app.use(express.json());
+
+// Main Route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Learnig Express Js')
 })
 
+// Get All Todos Data
+app.get('/all-todos', (req: Request, res: Response) => {
+  const data = fs.readFileSync(dataFilePath, { encoding: 'utf-8' });
+  res.json(data);
+})
+
+// Post Or Create New Todos
+app.post('/create-todo', (req: Request, res: Response) => {
+  const { title, body } = req.body;
+  console.log(title, body);
+  res.send("todo created");
+})
 
 export default app;
